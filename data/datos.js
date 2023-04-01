60851a40444c8801899114f5c4f039bedd325b3b
justinaalric <jalricferre@udesa.edu.ar>
2023-04-01 17:35:59 -0300

    detalles en data base de productos

--- data/datos.js
+++ data/datos.js
@@ -8,61 +8,62 @@ let datos = {
         {
             imagen: ss,
             nombre: 'Nike Blazer Jumbo',
-            descripcion: ss,
+            descripcion: "Alabadas por muchos por su aspecto y su sensación de resistencia, el básico de armario se actualiza con las Nike Blazer Mid ' 77 Jumbo.Aprovecha el look clásico que más te gusta, que ahora cuenta con un talón elástico con textura de pana y unas presillas grandes para que puedas ponértelas y quitártelas fácilmente.El diseño Swoosh extragrande y los cordones enormes añaden un toque divertido"
+            , 
             fechaCarga: ss
         },
         {
             imagen: ss,
             nombre: 'Converse Chuck 70s',
-            descripcion: ss,
+            descripcion: "Para un estilo en el que puedas confiar, las zapatillas Chuck Taylor All Star son un elemento básico de la moda. Las icónicas siluetas de caña alta y baja se mantienen fieles al legado. Esta es la opción que combina con todo, que no te vas a cansar",
             fechaCarga: ss
         },
         {
             imagen: ss,
             nombre: 'Golden Goose Superstar',
-            descripcion: ss,
+            descripcion: "Las zapatillas 'Superstar' de Golden Goose han sido cepilladas y enceradas a mano para darles la apariencia y la sensación de unos viejos favoritos. Confeccionadas en cuero suave, este par blanco está decorado con la aplicación de estrella característica de la marca. Úsalas durante el fin de semana con una camiseta y unos jeans.",
             fechaCarga: ss
         },
         {
             imagen: ss,
             nombre: 'Nike Jordans mid',
-            descripcion: ss,
+            descripcion: "Inspiradas en las AJ1 originales, las Air Jordan 1 Mid ofrecen a los aficionados la oportunidad de seguir los pasos de MJ. El nuevo color ribetea los materiales clásicos e impecables",
             fechaCarga: ss
         },
         {
             imagen: ss,
             nombre: 'Veja Esplar Low',
-            descripcion: ss,
+            descripcion: "Diseñado en una horma de zapato unisex utilizando tallas europeas, forro técnico hecho de botellas de plástico 100% recicladas. Un estilo minimalista y de líneas limpias, estas zapatillas Veja son un par que combina con todo",
             fechaCarga: ss
         },
         {
             imagen: ss,
             nombre: 'Nike Blazer Low',
-            descripcion: ss,
+            descripcion: "Inspiradas en el look clásico de baloncesto, las zapatillas Nike Blazer Low cuentan con una parte superior de piel y una zona del tobillo de corte bajo para ofrecer comodidad y durabilidad. Confección en autoclave que une la suela exterior con la mediasuela. Para ir al trabajo o disfrutar del fin de semana, luce un look estilizado para todos los días de la semana.",
             fechaCarga: ss
         },
         {
             imagen: ss,
             nombre: 'Z&V High Flash White',
-            descripcion: ss,
+            descripcion: "Icónicas y esenciales, las zapatillas altas ZV1747 High Flash están disponibles en piel blanca tono sobre tono. Plantilla cuña de 2,5 cm.Este modelo calza normal, te recomendamos que cojas tu talla habitua",
             fechaCarga: ss
         },
         {
             imagen: ss,
             nombre: 'Z&V High Flash Canvas',
-            descripcion: ss,
+            descripcion: "Las zapatillas de alta gama ZV1747 High Flash están disponibles esta temporada en lona de algodón. Presentan bordes crudos, destellos pespunteados, cordones contrastantes y cierre de cremallera.",
             fechaCarga: ss
         },
         {
             imagen: ss,
             nombre: 'Off White Orange',
-            descripcion: ss,
+            descripcion: "Zapatillas Off-Court 3.0 de caña alta en blanco con el logotipo de Off-White™ en un lado y un parche de flecha tonal en el otro lado. Detalles de etiquetas naranjas. Suela de goma blanca. Cierre de cordones blancos.",
             fechaCarga: ss
         },
         {
             imagen: ss,
             nombre: 'Adidas Samba',
-            descripcion: ss,
+            descripcion: "Inspiradas en los colores de Argentina, estas zapatillas adidas Samba Team combinan las 3 tiras blancas con una parte superior de cuero sintético verde bosque. El escudo del equipo en la lengüeta muestra el orgullo que sentís cada vez que te las ponés. Además, si las das vuelta vas a ver la clásica suela exterior de caucho desarrollada por primera vez para el entrenamiento de fútbol en 1949.",
             fechaCarga: ss
         }
     ],
