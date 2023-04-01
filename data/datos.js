db0371a591f1d08dfe836936bf5053f115b6d72c
Francisco Manoukian <franciscomanoukian@Franciscos-MacBook-Air.local>
2023-04-01 16:44:28 -0300

    Creo modulo de datos

---
+++ data/datos.js
@@ -0,0 +1,83 @@
+let datos = {
+    usuario: {
+        nombreUsuario: DelfiAlric,
+        clave: Programacion,
+        recordarme: true // Va o no va??????
+    },
+    productos: [
+        {
+            imagen: ss,
+            nombre: 'Nike Blazer',
+            descripcion: ss,
+            fechaCarga: ss
+        },
+        {
+            imagen: ss,
+            nombre: 'Converse Chuck 70s',
+            descripcion: ss,
+            fechaCarga: ss
+        },
+        {
+            imagen: ss,
+            nombre: 'Golden Goose Superstar',
+            descripcion: ss,
+            fechaCarga: ss
+        },
+        {
+            imagen: ss,
+            nombre: 'Nike Jordans mid',
+            descripcion: ss,
+            fechaCarga: ss
+        },
+        {
+            imagen: ss,
+            nombre: 'Veja Esplar Low',
+            descripcion: ss,
+            fechaCarga: ss
+        },
+        {
+            imagen: ss,
+            nombre: 'Adidas Forum',
+            descripcion: ss,
+            fechaCarga: ss
+        },
+        {
+            imagen: ss,
+            nombre: 'Z&V High Flash White',
+            descripcion: ss,
+            fechaCarga: ss
+        },
+        {
+            imagen: ss,
+            nombre: 'Z&V High Flash Canvas',
+            descripcion: ss,
+            fechaCarga: ss
+        },
+        {
+            imagen: ss,
+            nombre: 'Vans White Sk8',
+            descripcion: ss,
+            fechaCarga: ss
+        },
+        {
+            imagen: ss,
+            nombre: 'Reebok Classics',
+            descripcion: ss,
+            fechaCarga: ss
+        }
+    ],
+    comentarios: [
+        {
+            nombreUsuario: 'FranManoukian',
+            textoComentario: 'FAcheras',
+            imagenPerfil: null
+        },
+        {
+            nombreUsuario: 'JustiAlric',
+            textoComentario: 'Altas zapas!',
+            imagenPerfil: null
+        },
+    ]
+}
+
+module.exports = datos
