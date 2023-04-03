c19b2604055c0d533cc3df4b58928453a2109b19
Francisco Manoukian <franciscomanoukian@Franciscos-MacBook-Air.local>
2023-04-03 18:14:43 -0300

    Arreglo error en boton editar producto

--- routes/producto.js
+++ routes/producto.js
@@ -3,6 +3,7 @@ const productoController = require('../controllers/productoController');
 var router = express.Router();
 
 router.get('/detalle/id/:id?', productoController.detail);
+router.get('/edit/id', productoController.edit)
   
 module.exports = router;
   
