85bcf805757d9dc5ab455ba6e3c881eae8ad3ed2
delfinarawson <drawson@udesa.edu.ar>
2023-05-22 16:02:23 -0300

    cambiamos a post

--- controllers/registroController.js
+++ controllers/registroController.js
@@ -2,7 +2,6 @@ let registroController = {
     registrar: function(req, res) {
         res.render('register', {
             email: req.params.email
-            
         })
         console.log(email);
 
@@ -10,7 +9,7 @@ let registroController = {
     store: function(req, res){
         let form = req.body;
         let email = form.email;
-        return res.send(req.body)   //aca deberia usar un metodo de sequelize para guardar datos. Dentro del then deberia redireccionar a otra ruta
+        res.redirect('/login');  //aca deberia usar un metodo de sequelize para guardar datos. Dentro del then deberia redireccionar a otra ruta
 
 }}
 module.exports = registroController;
