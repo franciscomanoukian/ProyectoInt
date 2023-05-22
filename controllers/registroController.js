bff3e10510707a72c7a94c955ae38feae5706da6
justinaalric <jalricferre@udesa.edu.ar>
2023-05-22 15:53:42 -0300

    cambios registro

--- controllers/registroController.js
+++ controllers/registroController.js
@@ -1,9 +1,15 @@
 let registroController = {
     registrar: function(req, res) {
-        res.render('register');
+        res.render('register', {
+            email: req.params.email
+            
+        })
+        console.log(email);
+
     },
     store: function(req, res){
         let form = req.body;
+        let email = form.email;
         return res.send(req.body)   //aca deberia usar un metodo de sequelize para guardar datos. Dentro del then deberia redireccionar a otra ruta
     }
 }
