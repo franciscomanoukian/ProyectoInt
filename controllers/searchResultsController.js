16b29100fe2b81fb94d0ecc980a182b8d6f4afa0
justinaalric <jalricferre@udesa.edu.ar>
2023-05-29 17:32:24 -0300

    casi listo buscador, falta aplicarle select

--- controllers/searchResultsController.js
+++ controllers/searchResultsController.js
@@ -1,7 +1,23 @@
-let db = require("../data/datos");
+
+let db = require("../database/models");
 let searchResultsController = {
     showResults: function(req, res) {
-        res.render('search-results', {lista:db.productos});
+        let busqueda = req.query.search
+        let filtro = {
+            where: [{nombre: busqueda}]
+        }
+        db.Producto.findOne(filtro) 
+        .then(function(result){
+            if (result != null){
+                res.render('search-results', {lista: result});
+            }
+            else{
+                res.send("NO HEMOS ENCONTRADO RESULTADOS PARA SU BUSQUEDA, POR FAVOR INTENTELO DE NUEVO")
+            }
+        })
+        .catch( function(error){
+            console.log(error);
+        })
     }
 }
 module.exports = searchResultsController
