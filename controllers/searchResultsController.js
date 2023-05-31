23d6309f16cb98c95c822c1f1bf2683b5a34a66c
franciscomanoukian <fmanoukian@udesa.edu.ar>
2023-05-31 13:16:13 -0300

    Search results

--- controllers/searchResultsController.js
+++ controllers/searchResultsController.js
@@ -1,14 +1,22 @@
-
 let db = require("../database/models");
+let op = db.Sequelize.Op;
 let searchResultsController = {
     showResults: function(req, res) {
         let busqueda = req.query.search
         let filtro = {
-            where: [{nombre: busqueda}]
+            where: [{nombre: {[op.like]: `%${busqueda}%`}}],
+            order: [
+                ['createdAt', 'ASC']
+            ],
+            include: [
+                {association: "usuario"},{association: "comentarios"} // Incluye relacioness
+                ]
         }
-        db.Producto.findOne(filtro) 
+
+        db.Producto.findAll(filtro) 
         .then(function(result){
             if (result != null){
+                //res.send(result)
                 res.render('search-results', {lista: result});
             }
             else{
