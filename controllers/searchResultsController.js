8cf730327628e17f896e58a42aa2303e655c3519
justinaalric <jalricferre@udesa.edu.ar>
2023-06-01 17:07:54 -0300

    search results con descripcion lista!

--- controllers/searchResultsController.js
+++ controllers/searchResultsController.js
@@ -12,20 +12,43 @@ let searchResultsController = {
                 {association: "usuario"},{association: "comentarios"} // Incluye relacioness
                 ]
         }
+        let filtro2 = {
+            where: [{descripcion: {[op.like]: `%${busqueda}%`}}],
+            order: [
+                ['createdAt', 'ASC']
+            ],
+            include: [
+                {association: "usuario"},{association: "comentarios"} // Incluye relacioness
+                ]
+        }
 
         db.Producto.findAll(filtro) 
         .then(function(result){
-            if (result != null){
+            if (result.length != 0){
                 //res.send(result)
                 res.render('search-results', {lista: result});
             }
             else{
-                res.send("NO HEMOS ENCONTRADO RESULTADOS PARA SU BUSQUEDA, POR FAVOR INTENTELO DE NUEVO")
+                db.Producto.findAll(filtro2)
+                .then(function(result2){
+                    if (result2.length != 0){
+                        res.send(result2)
+                        //res.render('search-results', {lista: result2}); 
+                    }
+                    else{
+                        res.send("NO HEMOS ENCONTRADO RESULTADOS PARA SU BUSQUEDA, POR FAVOR INTENTELO DE NUEVO")
+                    }
+                })
+                .catch( function(error){
+                    console.log(error)
+                })
             }
         })
         .catch( function(error){
             console.log(error);
         })
+
+
     }
 }
 module.exports = searchResultsController
