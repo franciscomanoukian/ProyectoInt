774512c5569f98dbf3bb5f8353b1a85bc59c3536
justinaalric <jalricferre@udesa.edu.ar>
2023-06-05 12:27:27 -0300

    agregar producto, actualiza tabla

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -25,9 +25,19 @@ let productoController = {
         })
     },
     add: function (req, res) {
-        return res.render('product-add', {
-            datosUsuario: db.usuario
+        let form = req.body
+        let nombre = form.nombre
+        let imagen = form.imagen
+        let descripcion = form.descripcion
+        db.Producto.create({
+            imagen: imagen,
+            descripcion: descripcion,
+            nombre: nombre
         })
+
+        //return res.render('product-add', {
+        //    datosUsuario: db.usuario
+     //   })
     }
     
 }
