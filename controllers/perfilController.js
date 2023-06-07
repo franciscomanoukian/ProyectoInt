40467563e2cd931accc16c122246ec85c11a595c
franciscomanoukian <fmanoukian@udesa.edu.ar>
2023-06-07 14:23:42 -0300

    Profile obtiene datos desde session

--- controllers/perfilController.js
+++ controllers/perfilController.js
@@ -1,14 +1,30 @@
-let db = require("../data/datos"); //importando la lista, para mandarla a renderizar en mi objeto literal, para mostrar productos
+let db = require("../database/models"); //importando la lista, para mandarla a renderizar en mi objeto literal, para mostrar productos
 
 let perfilController = {
     detail: function(req, res){
-        return res.render('profile', {
-            datosUsuario: db.usuario, lista: db.productos, comentarios: db.comentarios
+        let idUsuario = req.session.user.id
+        
+        db.Usuario.findByPk(idUsuario, {
+            include: [
+                {association: "productos"} // Incluye relacioness
+                ]
+          })
+        .then(function(resultado){
+            //return res.send(resultado.productos) 
+            let productos = resultado.productos
+            
+            return res.render('profile', {lista: productos})
+            
         })
+        .catch( function(error){
+            console.log(error);
+        })
+
+        
     },
     edit: function (req, res) {
         return res.render('profile-edit', {
-            datosUsuario: db.usuario
+            
         })
     }
 }
