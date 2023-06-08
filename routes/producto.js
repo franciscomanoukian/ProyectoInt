16599d4440ed0b2b5d26c16cc4fc3446e861862e
delfinarawson <drawson@udesa.edu.ar>
2023-06-08 15:59:28 -0300

    PRODUCTO CONTROL DE ACCESOS

--- routes/producto.js
+++ routes/producto.js
@@ -5,6 +5,8 @@ var router = express.Router();
 router.get('/detalle/id/:id?', productoController.detail);
 router.get('/edit/id', productoController.edit)
 router.get('/add', productoController.add)
+
+router.post('/comment', productoController.comment)
   
 module.exports = router;
   
